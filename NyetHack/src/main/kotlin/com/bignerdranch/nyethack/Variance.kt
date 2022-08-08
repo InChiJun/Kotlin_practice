e0617ebc3992eb1552ab6cf45f79ef5fd27cc53a
Deniel_dev <clwnssla1@bible.ac.kr>
2022-08-08 20:39:26 +0900

    17장 학습 - 2022 8/8

---
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Variance.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.nyethack
+
+class Barrel<in T>(item: T)
+
+fun main(args: Array<String>) {
+    var fedoraBarrel: Barrel<Fedora> = Barrel(Fedora("평범한 중절모", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+
+    fedoraBarrel = lootBarrel
+ }
