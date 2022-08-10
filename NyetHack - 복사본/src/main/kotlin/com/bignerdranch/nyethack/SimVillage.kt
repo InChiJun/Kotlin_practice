1a1e2a02243073a6e2fe184df1e7fe9dd9482678
Daniel_dev <clwnssla1@bible.ac.kr>
2022-08-10 17:24:48 +0900

    복사본 삭제(챌린지는 그때 그때 생성해서 할 예정)

--- NyetHack - 복사본/src/main/kotlin/com/bignerdranch/nyethack/SimVillage.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val greetingFunction: (String) -> String = {
-        val currentYear = 2019
-        "Simvillage 방문을 환영합니다, $it 님! (copyRight $currentYear)"
-    }
-    println(greetingFunction("인치준"))
-}
